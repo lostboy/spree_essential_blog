fc92e7751c9bc94ca58a55526d0498fb58f8b0ea
lostboy <paul.crabtree@gmail.com>
2012-08-12 21:53:44 +0100

    switch to use s3 based images

--- app/models/spree/post_image.rb
+++ app/models/spree/post_image.rb
@@ -5,19 +5,17 @@ class Spree::PostImage < Spree::Asset
   validates_attachment_presence :attachment
 
   has_attached_file :attachment,
-    :styles => Proc.new{ |clip| clip.instance.attachment_sizes },
+    :styles => { :mini => '48x48>', :medium => '427x287>', :hero => '720x430#', :hub_thumb => "95x65#", :hub_info => "245x180#" },
     :default_style => :medium,
     :url => '/spree/posts/:id/:style/:basename.:extension',
     :path => ':rails_root/public/spree/posts/:id/:style/:basename.:extension'
 
-  def image_content?
-    attachment_content_type.to_s.match(/\/(jpeg|png|gif|tiff|x-photoshop)/)
-  end
-  
-  def attachment_sizes
-    hash = {}
-    hash.merge!(:mini => '48x48>', :small => '150x150>', :medium => '600x600>', :large => '950x700>') if image_content?
-    hash
+  if Spree::Config[:use_s3]
+    s3_creds = { :access_key_id => Spree::Config[:s3_access_key], :secret_access_key => Spree::Config[:s3_secret], :bucket => Spree::Config[:s3_bucket] }
+    self.attachment_definitions[:attachment][:storage] = :s3
+    self.attachment_definitions[:attachment][:s3_credentials] = s3_creds
+    self.attachment_definitions[:attachment][:s3_headers] = ActiveSupport::JSON.decode(Spree::Config[:s3_headers])
+    self.attachment_definitions[:attachment][:bucket] = Spree::Config[:s3_bucket]
   end
     
 end
